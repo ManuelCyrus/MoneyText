978fabf8729721bd656c8c3e875f09db7d20c9ec
manuelcyrus <manuelcyrus.mda@gmail.com>
2025-06-25 00:25:44 +0200

    Criando um arquivo de execução

---
+++ run.js
@@ -0,0 +1,17 @@
+
+const { porExtenso } = require('./index');
+
+const valor = parseInt(process.argv[2]);
+
+if (isNaN(valor)) {
+  console.log("⚠️  Use um número válido. Exemplo:");
+  console.log("   node run.js 1234");
+  process.exit(1);
+}
+
+try {
+  const texto = porExtenso(valor);
+  console.log(`→ ${valor} = "${texto}"`);
+} catch (err) {
+  console.error("Erro:", err.message);
+}
